58c8ae1446649e1ec9438ef952577e33c7a7c733
gunmm <924744097@qq.com>
2019-03-27 00:28:34 +0800

    【flutter】school页面Flex、Expand练习

--- flutter_show_demo1/lib/present/school_page.dart
+++ flutter_show_demo1/lib/present/school_page.dart
@@ -15,7 +15,62 @@ class SchoolPageState extends State<SchoolPageContent> {
         title: new Text("SchoolPage"),
       ),
 
-      body: new Text("SchoolPage"),
+      body: Column(
+        children: <Widget>[
+          Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 30,
+                  color: Colors.red,
+                ),
+              ),
+
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 30,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Container(
+                  color: Colors.red,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.green,
+                          child: Text("hello world "),
+                        ),
+
+
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Text("I am Jack "),
+                      ),
+
+                    ],
+                  ),
+                ),
+
+
+
+            ),
+
+          ),
+        ],
+      ),
     );
   }
 }
