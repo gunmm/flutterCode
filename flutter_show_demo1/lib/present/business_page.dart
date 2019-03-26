8d062ad5c3a8ce75b2f997cdbc5e8e8f6fe8aed0
gunmm <924744097@qq.com>
2019-03-26 18:09:53 +0800

    【flutter】布局类、容器类、基本listview

--- flutter_show_demo1/lib/present/business_page.dart
+++ flutter_show_demo1/lib/present/business_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 class BuinessPageContent extends StatefulWidget {
   @override
@@ -8,6 +9,31 @@ class BuinessPageContent extends StatefulWidget {
 class BuinessPageState extends State<BuinessPageContent> {
 
 
+  static const loadingTag = "##loading##";
+
+  var _words = <String>[loadingTag];
+
+  _loadData() {
+    Future.delayed(Duration(seconds: 2)).then((e) {
+      _words.insertAll(_words.length-1,
+          generateWordPairs().take(20).map((e) => e.asPascalCase).toList()
+      );
+
+      setState(() {
+
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadData();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold (
@@ -15,7 +41,45 @@ class BuinessPageState extends State<BuinessPageContent> {
         title: new Text("BuinessPage"),
       ),
 
-      body: new Text("BuinessPage"),
+      body: Column(
+        children: <Widget>[
+          ListTile(title:Text("商品列表")),
+          Expanded(
+              child: Scrollbar(
+                child: ListView.separated(
+                  itemCount: _words.length,
+                  itemBuilder: (context, index) {
+                    if(_words[index] == loadingTag) {
+                      if(_words.length-1 < 60) {
+                        _loadData();
+                        return Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(16),
+                          child: SizedBox(
+                              width: 24.0,
+                              height: 24.0,
+                              child: CircularProgressIndicator(strokeWidth: 2.0)
+                          ),
+                        );
+                      } else {
+                        return Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(16),
+                          child: Text("没有更多数据",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        );
+                      }
+                    }
+                    return ListTile(title: Text(_words[index]));
+                  },
+                  separatorBuilder: (context, index) => Divider(height: .0,),
+                ),
+              ),
+          ),
+
+        ],
+      ),
     );
   }
 }
--- flutter_show_demo1/lib/present/home_page.dart
+++ flutter_show_demo1/lib/present/home_page.dart
@@ -11,6 +11,10 @@ class HomePageContentState extends State<HomePageContent> {
 
   int _textCount = 1;
 
+  TextEditingController _textEditingController = new TextEditingController();
+  String _firstTextString = "";
+  String _secondTextString = "";
+
   _btnSelect(int currectCount) {
     setState(() {
       _textCount = currectCount;
@@ -38,73 +42,233 @@ class HomePageContentState extends State<HomePageContent> {
         ],
       ),
 
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  gradient: LinearGradient(
-                      colors: [Colors.red, Colors.orange[700]]),
-                  borderRadius: BorderRadius.circular(3),
-                  boxShadow: [ //阴影
-                    BoxShadow(
-                        color: Colors.black54,
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 4.0
+      body:SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    gradient: LinearGradient(
+                        colors: [Colors.red, Colors.orange[700]]),
+                    borderRadius: BorderRadius.circular(3),
+                    boxShadow: [ //阴影
+                      BoxShadow(
+                          color: Colors.black54,
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 4.0
+                      ),
+                    ],
+                  ),
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  child: Text("颠三倒四多说的是"*_textCount,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                Wrap(
+                  spacing: 8.0, // 主轴(水平)方向间距
+                  crossAxisAlignment: WrapCrossAlignment.start, //设置对齐方式
+                  verticalDirection: VerticalDirection.up,   //和crossAxisAlignment一起控制对齐方式
+                  children: <Widget>[
+                    SizedBox(
+                      height: 100,
+                      width: 150,
+                      child: RaisedButton(
+                        color: Colors.blue,
+                        child: Text("长度1",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        textColor: Colors.white,
+                        onPressed: () => {_btnSelect(1)},
+                      ),
+                    ),
+
+                    RaisedButton(
+                      color: Colors.blue,
+                      child: Text("长度2",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      onPressed: () => {_btnSelect(2)},
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      child: Text("长度3",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      onPressed: () => {_btnSelect(3)},
                     ),
                   ],
                 ),
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                child: Text("今天是个好日子"*_textCount,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
+
+
+
+                TextField(
+                  controller: _textEditingController,
+                  onChanged: (v) {
+                    setState(() {
+                      _secondTextString = _textEditingController.text;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: "用户名",
+                    hintText: "用户名或邮箱",
+                    prefixIcon: Icon(Icons.person),
                   ),
                 ),
-              ),
-              Wrap(
-                spacing: 8.0, // 主轴(水平)方向间距
-//                mainAxisSize: MainAxisSize.min,
-//                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  RaisedButton(
-                    color: Colors.blue,
-                    child: Text("长度1111111111111111111111111111111111111111",
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                    textColor: Colors.white,
-                    onPressed: () => {_btnSelect(1)},
 
-                  ),
-                  RaisedButton(
-                    color: Colors.blue,
-                    child: Text("长度2",
-                      style: TextStyle(
-                        fontSize: 18,
+                Wrap(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        gradient: LinearGradient(
+                            colors: [Colors.red, Colors.orange[700]]),
+                        borderRadius: BorderRadius.circular(3),
+                        boxShadow: [ //阴影
+                          BoxShadow(
+                              color: Colors.black54,
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 4.0
+                          ),
+                        ],
+                      ),
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      child: Text(_firstTextString,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
                       ),
                     ),
-                    textColor: Colors.white,
-                    onPressed: () => {_btnSelect(2)},
-                  ),
-                  RaisedButton(
-                    color: Colors.blue,
-                    child: Text("长度3",
-                      style: TextStyle(
-                        fontSize: 18,
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        gradient: LinearGradient(
+                            colors: [Colors.red, Colors.orange[700]]),
+                        borderRadius: BorderRadius.circular(3),
+                        boxShadow: [ //阴影
+                          BoxShadow(
+                              color: Colors.black54,
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 4.0
+                          ),
+                        ],
+                      ),
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      child: Text(_secondTextString,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
                       ),
                     ),
-                    textColor: Colors.white,
-                    onPressed: () => {_btnSelect(3)},
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+
+
+                Wrap(
+                  spacing: 20,
+                  children: <Widget>[
+                    RaisedButton(
+                      color: Colors.blue,
+                      child: Text("set",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          _firstTextString = _textEditingController.text;
+
+                        });
+                      },
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      child: Text("clear",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          _textEditingController.text = "";
+                          _firstTextString = "";
+                          _secondTextString = "";
+                        });
+                      },
+                    ),
+                  ],
+                ),
+
+                Wrap(
+//                textDirection: TextDirection.rtl,    文字方向   从左向右还是从右向左
+//                alignment: WrapAlignment.start,   //没试出来结果
+                  crossAxisAlignment: WrapCrossAlignment.center, //设置居中对齐
+                  spacing: 10,
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage("images/image1.png"),
+                      width: 100,
+                      height: 100,
+                      color: Colors.blue,
+                      colorBlendMode: BlendMode.difference,
+                    ),
+
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+//                      borderRadius: BorderRadius.circular(50),
+                        boxShadow: [ //阴影
+                          BoxShadow(
+                              color: Colors.black54,
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 4.0
+                          ),
+                        ],
+                      ),
+                      child: SizedBox(
+                        width: 50,
+                        height: 50,
+                        child:  ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: FadeInImage.assetNetwork(
+                            placeholder: "images/image2.png",
+                            image: "netUrl",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+
+              ],
+            ),
+
+
+
+
+
           ),
         ),
       ),
