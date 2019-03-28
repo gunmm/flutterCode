7cadeef92539fa483ed50b1f9292d96b9357f9d7
gunmm <924744097@qq.com>
2019-03-28 12:56:23 +0800

    【flutter】添加示例

--- flutter_show_demo1/lib/present/school_page.dart
+++ flutter_show_demo1/lib/present/school_page.dart
@@ -15,96 +15,32 @@ class SchoolPageState extends State<SchoolPageContent> {
         title: new Text("SchoolPage"),
       ),
 
-      body: ConstrainedBox(
-        constraints: BoxConstraints.expand(),
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Positioned(
-              left:10,
-              top: 10,
-              right: 10,
-              bottom: 10,
-              child: Container(
+      body: Center(
+        child: Container(
+          color: Colors.grey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: <Widget>[
+              Container(
                 color: Colors.red,
-                child: Text("sds sdsd 1"),
+                child: Text("我是第1个"),
               ),
-
-
-            ),
-            Text("I am Jack 3"),
-          ],
+              Container(
+                color: Colors.red,
+                child: Text("我是第2个第2个"),
+              ),
+              Container(
+                color: Colors.red,
+                child: Text("我是第3个第3个第3个"),
+              ),
+            ],
+          ),
         ),
+
       ),
 
-
-
-
-//      Column(
-//        children: <Widget>[
-//          Row(
-//            children: <Widget>[
-//              Expanded(
-//                flex: 2,
-//                child: Container(
-//                  height: 30,
-//                  color: Colors.red,
-//                ),
-//              ),
-//
-//              Expanded(
-//                flex: 2,
-//                child: Container(
-//                  height: 30,
-//                  color: Colors.green,
-//                ),
-//              ),
-//            ],
-//          ),
-//
-//          Expanded(
-//            flex: 1,
-//            child: Container(
-//              color: Colors.green,
-//              child: Column(
-//                mainAxisAlignment: MainAxisAlignment.center,
-//                children: <Widget>[
-//                  Text("hello world0 "),
-//                ],
-//              ),
-//            ),
-//          ),
-//
-//          Text("hello world1 "),
-//          Expanded(
-//            flex: 5,
-//            child: Container(
-//              color: Colors.red,
-//              child: Column(
-//                mainAxisAlignment: MainAxisAlignment.center,
-//                children: <Widget>[
-//                  Expanded(
-//                    flex: 1,
-//                    child: Container(
-//                      color: Colors.green,
-//                      child: Text("hello world2 "),
-//                    ),
-//
-//
-//                  ),
-//                  Expanded(
-//                    flex: 1,
-//                    child: Text("I am Jack 3"),
-//                  ),
-//
-//                ],
-//              ),
-//            ),
-//          ),
-//          Text("hello world4"),
-//
-//        ],
-//      ),
     );
   }
 }
